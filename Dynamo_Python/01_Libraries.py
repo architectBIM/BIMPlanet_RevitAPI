c04c80b8b3fe23e7d84987e7fd748a2e7da6f5ad
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-20 19:05:37 +0300

    Update 01_Libraries.py

--- Dynamo_Python/01_Libraries.py
+++ Dynamo_Python/01_Libraries.py
@@ -1,8 +1,9 @@
-# Importing clr (Common Language Runtime) module. This allows to import additional .NET assemblies and extension methods to out script
+""" Importing clr (Common Language Runtime) module. This allows to add additional .NET assemblies and extension methods to out script """
+
 import clr # Use dir(clr) command to see various methods and properties of clr
 
-# Adding various .NET assemblies (*.dll) to the reference list of clr. From this list you can import various namespaces and classes
-# Use clr.References command to see the whole list of references
+""" Adding various .NET assemblies (*.dll) to the reference list of clr. From this list you can import various namespaces and classes
+Use clr.References command to see the whole list of references """
 
 # References to Dynamo libraries
 clr.AddReference('ProtoGeometry') # DesignScript library for working with geometry
@@ -13,3 +14,5 @@ clr.AddReference('RevitServices') # Dynamo library for working with Revit docume
 # References to Revit API libraries
 clr.AddReference('RevitAPI') # Revit API library
 clr.AddReference('RevitAPIUI') # Revit API User Interface library
+
+""" Importing necessary classes and extension methods to our script """
