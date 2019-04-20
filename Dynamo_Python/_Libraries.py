5ea7d79022a8b16aa8bbd26617e4b5fe24b92447
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-20 12:52:59 +0300

    Update _Libraries.py

--- Dynamo_Python/_Libraries.py
+++ Dynamo_Python/_Libraries.py
@@ -1,3 +1,4 @@
-''' Importing Common Language Runtime module. This allows to import additional .NET assemblies and extension methods
-to out script '''
+# Importing Common Language Runtime module. This allows to import additional .NET assemblies and extension methods to out script
 import clr
+
+#Importing 
