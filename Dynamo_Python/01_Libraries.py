2d2ca8fad491cc277924e4ae2f3b4b11f8df15d3
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-20 18:22:25 +0300

    Update 01_Libraries.py

--- Dynamo_Python/01_Libraries.py
+++ Dynamo_Python/01_Libraries.py
@@ -1,6 +1,6 @@
 # Importing clr (Common Language Runtime) module. This allows to import additional .NET assemblies and extension methods to out script
-import clr
-# Importing RevitServices.dll assembly
-clr.AddReference('RevitServices') # AddReference method allows to add custom *.dll file to the reference list of clr
-from RevitServices.Persistence import DocumentManager as DM
-from RevitServices.Transactions import TransactionManager as TM
+import clr # To see various methods and properties of clr you can use dir(clr) command
+
+# Adding various .NET assemblies (*.dll) to the reference list of clr. From this list you can import various namespaces and classes.
+clr.AddReference('RevitServices') # Assembly for working with Revit document and committing transactions in it
+
