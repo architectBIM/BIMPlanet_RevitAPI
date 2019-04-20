7f9c2d55fc94412f067798123e7e9f8338ceb92b
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-20 16:15:27 +0300

    Update _Libraries.py

--- Dynamo_Python/_Libraries.py
+++ Dynamo_Python/_Libraries.py
@@ -1,4 +1,4 @@
 # Importing clr (Common Language Runtime) module. This allows to import additional .NET assemblies and extension methods to out script
 import clr
 # Importing RevitServices.dll assembly
-clr.AddReference('RevitServices') # AddReference method allows to add custom dll file to the reference list of clr
+clr.AddReference('RevitServices') # AddReference method allows to add custom *.dll file to the reference list of clr
