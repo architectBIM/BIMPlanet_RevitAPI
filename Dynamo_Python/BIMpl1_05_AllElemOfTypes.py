51b5bf39491cb9acb27901df98bd7cca4fb66908
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-27 19:24:35 +0300

    Create BIMpl1_05_AllElemOfTypes.py

---
+++ Dynamo_Python/BIMpl1_05_AllElemOfTypes.py
@@ -0,0 +1,28 @@
+"""
+BIM Planet No1. Знакомство с Dynamo (https://bimplanet.org/)
+Выборка элементов нескольких классов через коллектор и цикл
+MIT License, Copyright (c) 2019 Maxim Stepannikov
+"""
+
+import clr # Модуль для подгрузки .NET библиотек
+
+# Библиотеки Revit API
+clr.AddReference('RevitAPI') # Основная библиотека Revit API
+from Autodesk.Revit.DB import * # Импорт всех классов
+
+# Библиотеки Dynamo
+clr.AddReference('RevitServices') # Работа с документом и транзакциями
+from RevitServices.Persistence import DocumentManager as DM # Класс по работе с документом Revit
+
+doc = DM.Instance.CurrentDBDocument # Получение файла документа
+
+elems = [] # Создание пустого списка для будущих элементов
+classes = [WallType,Floor,FamilyInstance] # Создание списка необходимых классов
+
+# Создание цикла для получение элементов каждой из категорий данного списка
+for cl in classes:
+	
+	# Добавляем элементы в список. Если на выходе нужен двухмерный список - заменяем extend на append
+	elems.extend(FilteredElementCollector(doc).OfClass(cl).ToElements())
+	
+OUT = elems # Вывод элементов из узла Python Script
