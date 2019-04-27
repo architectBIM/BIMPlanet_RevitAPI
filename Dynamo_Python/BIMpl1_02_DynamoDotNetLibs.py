bb150132d4071eb0588ea6150d1ba3c540cf5848
Maxim Stepannikov <34342887+architectBIM@users.noreply.github.com>
2019-04-27 19:10:58 +0300

    Update BIMpl1_02_DynamoDotNetLibs.py

--- Dynamo_Python/BIMpl1_02_DynamoDotNetLibs.py
+++ Dynamo_Python/BIMpl1_02_DynamoDotNetLibs.py
@@ -1,3 +1,9 @@
+"""
+BIM Planet No1. Знакомство с Dynamo (https://stepik.org/course/50129)
+Шаблон библиотек для работы с нодами Dynamo внутри узла Python Script
+Copyright (c) 2019 Maxim Stepannikov, https://bimplanet.org
+"""
+
 clr.AddReference('ProtoGeometry') # Классы, аналогичные нодам Dynamo вкладки Geometry
 from Autodesk.DesignScript.Geometry import * # Указать классы для импорта
 
